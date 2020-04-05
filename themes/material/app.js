b75b8c25c50bcad32c485110c768190cf2a6cdf2
churongcon <verypro.vn@gmail.com>
2020-04-05 21:40:24 +0700

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -769,6 +769,8 @@ function file_code(path) {
             maxLines: Infinity
         });
     });
+	
+	
 }
 
 // 文件展示 视频 |mp4|webm|avi|
@@ -829,14 +831,60 @@ function file_audio(path) {
 function file_pdf(path) {
     var url = window.location.origin + path;
     var content = `
+	<script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>
+
+
 <div class="mdui-container-fluid">
 	<br>
-	
+	<h1>PDF.js 'Hello, world!' example</h1>
+
+<canvas id="the-canvas"></canvas><br>
 	<object width="400" height="50" data="${url}"></object>
 
 </div>
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
+	$('#content').html(content);
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+// Asynchronous download of PDF
+var loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(function(pdf) {
+  console.log('PDF loaded');
+  
+  // Fetch the first page
+  var pageNumber = 1;
+  pdf.getPage(pageNumber).then(function(page) {
+    console.log('Page loaded');
+    
+    var scale = 1.5;
+    var viewport = page.getViewport({scale: scale});
+
+    // Prepare canvas using PDF page dimensions
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+    renderTask.promise.then(function () {
+      console.log('Page rendered');
+    });
+  });
+}, function (reason) {
+  // PDF loading error
+  console.error(reason);
+});	
 }
 
 // 图片展示
