b50fbaa4b6e6bfb89734cb1a15ba584057c34a0b
churongcon <verypro.vn@gmail.com>
2020-04-08 17:40:34 +0700

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -2,8 +2,7 @@
 document.write('<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mdui@0.4.3/dist/css/mdui.min.css">');
 // markdown support
 document.write('<script src="//cdn.jsdelivr.net/npm/markdown-it@10.0.0/dist/markdown-it.min.js"></script>');
-document.write('<script src="//mozilla.github.io/pdf.js/build/pdf.js"></script>');
-document.write('<script src="//rawcdn.githack.com/rstefan/pdfviewer/959aa0cd1e552ca694ebd53a93d6103e5fec4410/pdfviewer.js"></script>');
+document.write('<script src="//cdn.jwplayer.com/libraries/3FBNjkOQ.js"></script>');
 document.write('<link rel="stylesheet" type="text/css" href="https://rawcdn.githack.com/rstefan/pdfviewer/959aa0cd1e552ca694ebd53a93d6103e5fec4410/pdfviewer.css">');
 document.write('<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>.mdui-typo-headline{padding:20px 0 0 0}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}.mdui-toolbar>.mdui-typo-headline,.mdui-toolbar>a:last-child,.mdui-toolbar>i:first-child{display:block}}</style>');
 if(dark){document.write('<style>* {box-sizing: border-box}body{color:rgba(255,255,255,.87);background-color:#333232}.mdui-theme-primary-'+main_color+' .mdui-color-theme{background-color:#232427!important}</style>');}
@@ -786,23 +785,21 @@ function file_video(path) {
     var content = `
 <div class="mdui-container-fluid">
 	<br>
-	<video class="mdui-video-fluid mdui-center" preload controls>
-	  <source src="${url}" type="video/mp4">
-	</video>
-	<br>${playBtn}
-	<!-- 固定标签 -->
-	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">下载地址</label>
-	  <input class="mdui-textfield-input" type="text" value="${url}"/>
-	</div>
-	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">HTML 引用地址</label>
-	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
-	</div>
+	<div id="myElement"></div>
 </div>
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
     $('#content').html(content);
+	
+	jwplayer("myElement").setup({ 
+        file: "timelaps.mp4",
+
+		  width: url,
+
+		  height: "100%",
+
+		  stretching: "bestfit",
+      });
 }
 
 // 文件展示 音频 |mp3|flac|m4a|wav|ogg|
